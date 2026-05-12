517a376f131dd53cf85606e709630e966ceabd0e
Akaio181 <158824828+Akaio181@users.noreply.github.com>
2026-05-12 13:12:24 +0500

    Update TaskControllerTest.java

--- src/test/java/com/example/taskapi/controller/TaskControllerTest.java
+++ src/test/java/com/example/taskapi/controller/TaskControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 import java.util.List;
 import java.util.Optional;
